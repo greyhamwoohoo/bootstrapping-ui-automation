036b9df11ea578d1982e1e4b3f009c88ce2651ca
greyhamwoohoo <46085273+greyhamwoohoo@users.noreply.github.com>
2020-08-14 00:50:00 +1000

    DataSourceAttribute and parsing tests for configuration files

---
+++ src/NetCore-Selenium/TheInternet.Common.UnitTests/Runtime/RuntimeFileTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Linq;
+using TheInternet.Common.DataSource;
+
+namespace TheInternet.Common.UnitTests.Runtime
+{
+    [TestClass]
+    public class RuntimeFileTests
+    {
+        [TestClass]
+        public class WhenRuntimeFilesAreDeployed : TestBase
+        {
+            [TestMethod]
+            public void ThereAreManyFiles()
+            {
+                DiscoverFilesAtPath("Runtime", "*.json").Count().Should().BeGreaterThan(10, because: "we will be parsing all Json files. ");
+            }
+
+            [TestMethod]
+            [RuntimeFilePathDataSource(relativePath: "Runtime", pattern: "*.json")]
+            public void AllRuntimeFilesCanBeParsed(string runtimeFilePath)
+            {
+                var content = System.IO.File.ReadAllText(runtimeFilePath);
+
+                _ = JsonConvert.DeserializeObject(content);
+            }
+        }
+    }
+}
---
+++ src/NetCore-Selenium/TheInternet.Common/DataSource/RuntimeFilePathDataSource.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace TheInternet.Common.DataSource
+{
+    /// <summary>
+    /// DataSource that will dynamically discover the configuration files relative to the .Common assembly.
+    /// </summary>
+    public class RuntimeFilePathDataSource : Attribute, ITestDataSource
+    {
+        private readonly string Path;
+        private readonly string RelativePath;
+        private readonly string Pattern;
+
+        public RuntimeFilePathDataSource(string relativePath, string pattern)
+        {
+            RelativePath = relativePath ?? throw new System.ArgumentNullException(nameof(relativePath));
+            Pattern = pattern ?? throw new System.ArgumentNullException(nameof(pattern));
+
+            Path = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(typeof(RuntimeFilePathDataSource).Assembly.Location), relativePath);
+        }
+
+        public IEnumerable<object[]> GetData(MethodInfo methodInfo)
+        {
+            if (!System.IO.Directory.Exists(Path)) yield break;
+
+            var runtimeFilePaths = System.IO.Directory.GetFiles(Path, Pattern, System.IO.SearchOption.AllDirectories);
+
+            if (runtimeFilePaths.Length == 0) yield break;
+
+            foreach (var runtimeFilePath in runtimeFilePaths)
+            {
+                yield return new object[] { runtimeFilePath };
+            };
+        }
+
+        public string GetDisplayName(MethodInfo methodInfo, object[] data)
+        {
+            var runtimeFilePath = $"{data[0]}";
+
+            var name = System.IO.Path.GetRelativePath(Path, runtimeFilePath);
+
+            return name;
+        }
+    }
+}
