80563967a31793b71a648ca09807c3cb79c17d1c
unknown <46085273+greyhamwoohoo@users.noreply.github.com>
2021-09-11 22:25:49 +1000

    Removing Selenium4.0-alpha3

--- src/ZZZ-Obsolete-NetCore-Selenium-4.0-alpha3/TheInternet.Common/ExecutionContext/Runtime/BrowserSettings/Contracts/IBrowserProperties.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TheInternet.Common.ExecutionContext.Runtime.BrowserSettings.Contracts
-{
-    public interface IBrowserProperties
-    {
-        string Name { get; }
-        BrowserSettingsBase BrowserSettings { get; }
-    }
-}
